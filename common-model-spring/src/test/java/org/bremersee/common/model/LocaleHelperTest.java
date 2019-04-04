cc9572ebfce3a4c4055deaf355c7f7030ab8ea4f
Christian Bremer <christian.bremer@cstx.de>
2019-04-04 17:56:04 +0200

    Dev

---
+++ common-model-spring/src/test/java/org/bremersee/common/model/LocaleHelperTest.java
@@ -0,0 +1,123 @@
+package org.bremersee.common.model;
+
+import java.util.Arrays;
+import java.util.Locale;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * The locale helper test.
+ *
+ * @author Christian Bremer
+ */
+public class LocaleHelperTest {
+
+  /**
+   * Gets supported locales.
+   */
+  @Test
+  public void getSupportedLocales() {
+    Locale[] expected = Arrays.stream(JavaLocale.values())
+        .map(LocaleHelper::toLocale)
+        .toArray(Locale[]::new);
+    Locale[] actual = LocaleHelper.getSupportedLocales();
+    Assert.assertArrayEquals(expected, actual);
+
+    expected = new Locale[]{Locale.GERMANY};
+    actual = LocaleHelper.getSupportedLocales(Locale.GERMANY);
+    Assert.assertArrayEquals(expected, actual);
+  }
+
+  /**
+   * Build http language descriptions.
+   */
+  @Test
+  public void buildHttpLanguageDescriptions() {
+    HttpLanguageTagDescriptions expected = new HttpLanguageTagDescriptions()
+        .addEntriesItem(LocaleHelper.buildHttpLanguageDescription(Locale.JAPANESE, Locale.GERMAN));
+    HttpLanguageTagDescriptions actual = LocaleHelper.buildHttpLanguageDescriptions(
+        Locale.JAPANESE, Locale.GERMAN);
+    Assert.assertNotNull(actual);
+    Assert.assertEquals(expected, actual);
+  }
+
+  /**
+   * Build http language description.
+   */
+  @Test
+  public void buildHttpLanguageDescription() {
+    HttpLanguageTagDescription expected = new HttpLanguageTagDescription()
+        .tag(HttpLanguageTag.IT_IT)
+        .description(Locale.ITALY.getDisplayLanguage(Locale.GERMAN)
+            + " (" + Locale.ITALY.getDisplayCountry(Locale.GERMAN) + ")");
+    HttpLanguageTagDescription actual = LocaleHelper.buildHttpLanguageDescription(
+        Locale.GERMAN, Locale.ITALY);
+    Assert.assertNotNull(actual);
+    Assert.assertEquals(expected, actual);
+  }
+
+  /**
+   * Build java locale descriptions.
+   */
+  @Test
+  public void buildJavaLocaleDescriptions() {
+    JavaLocaleDescriptions expected = new JavaLocaleDescriptions()
+        .addEntriesItem(LocaleHelper.buildJavaLocaleDescription(Locale.JAPANESE, Locale.GERMAN));
+    JavaLocaleDescriptions actual = LocaleHelper.buildJavaLocaleDescriptions(
+        Locale.JAPANESE, Locale.GERMAN);
+    Assert.assertNotNull(actual);
+    Assert.assertEquals(expected, actual);
+  }
+
+  /**
+   * Build java locale description.
+   */
+  @Test
+  public void buildJavaLocaleDescription() {
+    JavaLocaleDescription expected = new JavaLocaleDescription()
+        .locale(JavaLocale.IT_IT)
+        .description(Locale.ITALY.getDisplayLanguage(Locale.GERMAN)
+            + " (" + Locale.ITALY.getDisplayCountry(Locale.GERMAN) + ")");
+    JavaLocaleDescription actual = LocaleHelper.buildJavaLocaleDescription(
+        Locale.GERMAN, Locale.ITALY);
+    Assert.assertNotNull(actual);
+    Assert.assertEquals(expected, actual);
+  }
+
+  /**
+   * To java locale.
+   */
+  @Test
+  public void toJavaLocale() {
+    JavaLocale expected = JavaLocale.FR_CA;
+    JavaLocale actual = LocaleHelper.toJavaLocale(Locale.CANADA_FRENCH);
+    Assert.assertNotNull(actual);
+    Assert.assertEquals(expected, actual);
+  }
+
+  /**
+   * To http language tag.
+   */
+  @Test
+  public void toHttpLanguageTag() {
+    HttpLanguageTag expected = HttpLanguageTag.EN_GB;
+    HttpLanguageTag actual = LocaleHelper.toHttpLanguageTag(new Locale("en", "GB"));
+    Assert.assertNotNull(actual);
+    Assert.assertEquals(expected, actual);
+  }
+
+  /**
+   * To locale.
+   */
+  @Test
+  public void toLocale() {
+    Locale expected = Locale.FRANCE;
+    Locale actual = LocaleHelper.toLocale(JavaLocale.FR_FR);
+    Assert.assertNotNull(actual);
+    Assert.assertEquals(expected, actual);
+    actual = LocaleHelper.toLocale(HttpLanguageTag.FR_FR);
+    Assert.assertNotNull(actual);
+    Assert.assertEquals(expected, actual);
+  }
+
+}
